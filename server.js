11e1f21f6bb044b57215d34debd614209b57ec1a
VBTX <jv.svedaite@gmail.com>
2019-11-29 21:17:53 -0600

    set up environment configs and server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+//in order to start a server we need to listen to a port
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
