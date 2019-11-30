55497931a1590381edcce30050bdaabe2461be88
VBTX <jv.svedaite@gmail.com>
2019-11-29 21:31:39 -0600

    set up initial route

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+app.get("/api/v1/subjects", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all subjects" });
+});
+
 //in order to start a server we need to listen to a port
 const PORT = process.env.PORT || 5000;
 
